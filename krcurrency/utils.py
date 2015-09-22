eba354cfaa96754b814daeb7fa453e538b07a879
ssut <ssut@ssut.me>
2015-09-22 15:20:20 +0900

    Add tofloat function that transforms from any float-based string into float

--- krcurrency/utils.py
+++ krcurrency/utils.py
@@ -5,7 +5,7 @@
 from bs4 import BeautifulSoup as BS
 import requests
 
-__all__ = 'request',
+__all__ = 'request', 'tofloat',
 
 def request(url, encoding='utf-8', parselib='lxml'):
     """url로 요청한 후 돌려받은 값을 BeautifulSoup 객체로 변환해서 반환합니다.
@@ -21,3 +21,12 @@ def request(url, encoding='utf-8', parselib='lxml'):
     except Exception as e:
         pass
     return soup
+
+def tofloat(text):
+    transformed = None
+    try:
+        text = text.replace(',', '')
+        transformed = float(text)
+    except:
+        pass
+    return transformed
